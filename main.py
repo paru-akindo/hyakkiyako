8813280a516f95d080311d9b4dbdd0d906b22c63
paru-akindo <akindo125paru@gmail.com>
2025-05-20 15:23:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -226,10 +226,10 @@ class MergeGameSimulator:
 # ----------------------------
 # Streamlit アプリ本体
 # ----------------------------
-st.title("Merge Game Simulator v2")
+st.title("百鬼夜行")
 
 # 盤面の入力方法選択
-input_method = st.radio("盤面の入力方法を選択", ("グリッド入力", "カンマ区切りテキスト入力"))
+input_method = st.radio("盤面の入力方法を選択", ("カンマ区切りテキスト入力", "グリッド入力"))
 
 # セッション状態の初期化
 if "grid_board_values" not in st.session_state:
@@ -293,7 +293,7 @@ if board is not None:
     st.subheader("入力された盤面")
     st.dataframe(format_board(board))
 
-simulate_button = st.button("Simulate (シミュレーション開始)")
+simulate_button = st.button("実行")
 
 if simulate_button:
     if board is None:
